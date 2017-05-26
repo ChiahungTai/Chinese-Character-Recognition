475c56819231f3636ca6d61d266d39966d3c5015
ctai <glaxy.tai@gmail.com>
2017-05-26 12:18:28 +0800

    Can work on my PC.

--- chinese_character_recognition_bn.py
+++ chinese_character_recognition_bn.py
@@ -93,6 +93,65 @@ def build_graph(top_k):
     images = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1], name='image_batch')
     labels = tf.placeholder(dtype=tf.int64, shape=[None], name='label_batch')
     is_training = tf.placeholder(dtype=tf.bool, shape=[], name='train_flag')
+#    with tf.device('/gpu:0'):
+    with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                        normalizer_fn=slim.batch_norm,
+                        normalizer_params={'is_training': is_training}):
+        conv3_1 = slim.conv2d(images, 64, [3, 3], 1, padding='SAME', scope='conv3_1')
+        max_pool_1 = slim.max_pool2d(conv3_1, [2, 2], [2, 2], padding='SAME', scope='pool1')
+        conv3_2 = slim.conv2d(max_pool_1, 128, [3, 3], padding='SAME', scope='conv3_2')
+        max_pool_2 = slim.max_pool2d(conv3_2, [2, 2], [2, 2], padding='SAME', scope='pool2')
+        conv3_3 = slim.conv2d(max_pool_2, 256, [3, 3], padding='SAME', scope='conv3_3')
+        max_pool_3 = slim.max_pool2d(conv3_3, [2, 2], [2, 2], padding='SAME', scope='pool3')
+        conv3_4 = slim.conv2d(max_pool_3, 512, [3, 3], padding='SAME', scope='conv3_4')
+        conv3_5 = slim.conv2d(conv3_4, 512, [3, 3], padding='SAME', scope='conv3_5')
+        max_pool_4 = slim.max_pool2d(conv3_5, [2, 2], [2, 2], padding='SAME', scope='pool4')
+
+        flatten = slim.flatten(max_pool_4)
+        fc1 = slim.fully_connected(slim.dropout(flatten, keep_prob), 1024,
+                                   activation_fn=tf.nn.relu, scope='fc1')
+        logits = slim.fully_connected(slim.dropout(fc1, keep_prob), FLAGS.charset_size, activation_fn=None,
+                                      scope='fc2')
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(logits, 1), labels), tf.float32))
+
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    if update_ops:
+        updates = tf.group(*update_ops)
+        loss = control_flow_ops.with_dependencies([updates], loss)
+
+    global_step = tf.get_variable("step", [], initializer=tf.constant_initializer(0.0), trainable=False)
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+    train_op = slim.learning.create_train_op(loss, optimizer, global_step=global_step)
+    probabilities = tf.nn.softmax(logits)
+
+    tf.summary.scalar('loss', loss)
+    tf.summary.scalar('accuracy', accuracy)
+    merged_summary_op = tf.summary.merge_all()
+    predicted_val_top_k, predicted_index_top_k = tf.nn.top_k(probabilities, k=top_k, name="top_k")
+    accuracy_in_top_k = tf.reduce_mean(tf.cast(tf.nn.in_top_k(probabilities, labels, top_k), tf.float32))
+
+    return {'images': images,
+            'labels': labels,
+            'keep_prob': keep_prob,
+            'top_k': top_k,
+            'global_step': global_step,
+            'train_op': train_op,
+            'loss': loss,
+            'is_training': is_training,
+            'accuracy': accuracy,
+            'accuracy_top_k': accuracy_in_top_k,
+            'merged_summary_op': merged_summary_op,
+            'predicted_distribution': probabilities,
+            'predicted_index_top_k': predicted_index_top_k,
+            'predicted_val_top_k': predicted_val_top_k}
+
+
+def build_graph1(top_k):
+    keep_prob = tf.placeholder(dtype=tf.float32, shape=[], name='keep_prob')
+    images = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1], name='image_batch')
+    labels = tf.placeholder(dtype=tf.int64, shape=[None], name='label_batch')
+    is_training = tf.placeholder(dtype=tf.bool, shape=[], name='train_flag')
     with tf.device('/gpu:0'):
         with slim.arg_scope([slim.conv2d, slim.fully_connected],
                             normalizer_fn=slim.batch_norm,
@@ -206,14 +265,17 @@ def train():
                     logger.info('===============Eval a batch=======================')
                 if step % FLAGS.save_steps == 1:
                     logger.info('Save the ckpt of {0}'.format(step))
+                    tf.train.write_graph(sess.graph.as_graph_def(), "./models/", model_name + str(step) + ".pb", as_text=False)
                     saver.save(sess, os.path.join(FLAGS.checkpoint_dir, model_name),
                                global_step=graph['global_step'])
+
         except tf.errors.OutOfRangeError:
             logger.info('==================Train Finished================')
             saver.save(sess, os.path.join(FLAGS.checkpoint_dir, model_name), global_step=graph['global_step'])
         finally:
             coord.request_stop()
         coord.join(threads)
+    return {'prob': final_predict_val, 'indices': final_predict_index, 'groundtruth': groundtruth}
 
 
 def validation():
@@ -224,7 +286,7 @@ def validation():
     final_predict_index = []
     groundtruth = []
 
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True))  as sess:
         test_images, test_labels = test_feeder.input_pipeline(batch_size=FLAGS.batch_size, num_epochs=1)
         graph = build_graph(top_k=3)
         saver = tf.train.Saver()
@@ -283,7 +345,7 @@ def inference(image):
     temp_image = temp_image.resize((FLAGS.image_size, FLAGS.image_size), Image.ANTIALIAS)
     temp_image = np.asarray(temp_image) / 255.0
     temp_image = temp_image.reshape([-1, 64, 64, 1])
-    with tf.Session() as sess:
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True))  as sess:
         logger.info('========start inference============')
         # images = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1])
         # Pass a shadow label 0. This label will not affect the computation graph.
@@ -311,7 +373,7 @@ def main(_):
             pickle.dump(dct, f)
         logger.info('Write file ends')
     elif FLAGS.mode == 'inference':
-        image_path = '../data/test/00190/13320.png'
+        image_path = '../data/test/00031/70110.png'
         final_predict_val, final_predict_index = inference(image_path)
         logger.info('the result info label {0} predict index {1} predict_val {2}'.format(190, final_predict_index,
                                                                                          final_predict_val))
